4671b52b312aca28a326b7e18181f6929e08b99a
MarCnu <marceau.cnudde@gmail.com>
2023-11-05 14:44:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     url='https://github.com/MarCnu/gsheet_ml_scheduler.git',
     author='MarCnu',
-    description='A simple scheduler that allows multiple instances of Colab to fetch machine learning run configs from a Google Docs Sheets',
+    description='A simple experiment scheduler that allows multiple instances of Colab to fetch machine learning experiment metaparameters by reading/writing in a Sheet from Google Docs Sheets.',
     packages=['gsheet_ml_scheduler'],    
     install_requires=['pandas', 'gspread', 'google-auth'],
 )
