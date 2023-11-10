ca564e45ff188c5a0862295139b6d6abed58bcee
MarCnu <marceau.cnudde@gmail.com>
2023-11-10 15:33:34 +0100

    Update test_convert_str.py

--- tests/test_convert_str.py
+++ tests/test_convert_str.py
@@ -28,5 +28,29 @@ class TestConvertStr(unittest.TestCase):
 
         self.assertEqual(GSheetMLScheduler.convert_str_to_bool_int_float_str("Whatever"), "Whatever")
 
+    def test_convert_str_to_bool_int_float_str_type(self):
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str(""), str)
+
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("TRUE"), bool)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("True"), bool)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("true"), bool)
+
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("FALSE"), bool)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("False"), bool)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("false"), bool)
+
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("42"), int)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("0"), int)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("-42"), int)
+
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("0.01"), float)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str(".01"), float)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("0.0"), float)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("-0.01"), float)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("-2e-4"), float)
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("-2E-4"), float)
+
+        self.assertIsInstance(GSheetMLScheduler.convert_str_to_bool_int_float_str("Whatever"), str)
+
 if __name__ == '__main__':
     unittest.main()
